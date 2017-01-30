f841712a79dcb3e41e96db9467edc99b755d2b38
Hoai Nam Duc Tran <Hoai Nam Duc Tran>
2017-01-30 18:59:39 +0000

    layout and activity for future usage

---
+++ app/src/main/java/com/example/sareenaith/theeventfinder/SearchActivity.java
@@ -0,0 +1,17 @@
+package com.example.sareenaith.theeventfinder;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Hoai Nam Duc Tran on 30/01/2017.
+ */
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
