ee66f8b66044675cb0f33835889ec40ec7a4e059
gustafhalldor <gustafh@gmail.com>
2017-03-27 00:07:22 +0000

    Changing app name to Eventure

--- app/src/main/java/com/example/sareenaith/theeventfinder/Config.java
+++ app/src/main/java/com/example/sareenaith/theeventfinder/Config.java
@@ -1,7 +1,8 @@
 package com.example.sareenaith.theeventfinder;
 
 /**
- * Created by geelo on 18-Mar-17.
+ * Server is hosted on Heroku so if you want to connect to live data you use that URL, else you
+ * need to figure out your local IP if you wish to develop against a local server instead.
  */
 
 public class Config {
